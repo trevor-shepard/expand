42ad593968cd6378433a0043b1b522d3a58e1580
trevor-shepard <trevorbshepard@gmail.com>
2019-01-15 13:41:40 -0800

    Add explaniation and instructions

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 import Game from './game';
 
 document.addEventListener("DOMContentLoaded", () => {
+    let playButton = document.getElementById('play-button')
+    playButton.addEventListener('click', () => {
+        let modal = document.getElementById('explaination-modal')
+        modal.classList.toggle('hidden')
+    })
+    
+    
+    
     let game = new Game();
+
+
     game.start()
 })
