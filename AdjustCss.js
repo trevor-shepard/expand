6ca8939b910dd163d2b8f4cd77f786c1f8e36407
trevor-shepard <trevorbshepard@gmail.com>
2019-01-30 13:07:31 -0800

    Dynamic Sizing

---
+++ AdjustCss.js
@@ -0,0 +1,26 @@
+export default class CssRule  {
+    constructor(sheetName) {
+        this.styleSheet
+        for (let styleSheet of document.styleSheets) {
+            if (styleSheet.href.includes(sheetName)) {
+                this.styleSheet = styleSheet
+            }
+        }
+        
+        this.adjust = this.adjust.bind(this)
+    }
+
+    adjust(cssIdentifyer, changeStyle, changeValue) {
+        debugger
+        for (let rule of this.styleSheet.rules) {
+            if (rule.selectorText === cssIdentifyer) {
+                rule.style[changeStyle] = changeValue;
+            }
+        }
+    }
+
+
+}
+
+
+
